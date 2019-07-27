d68b225d8e2b674c91b1071940c930a447814413
Cory <coadams9@gmail.com>
2019-07-26 23:06:47 -0400

    working through trees

---
+++ algorithms/tree.js
@@ -0,0 +1,44 @@
+class Node {
+    constructor(data) {
+        this.data = data
+        this.children = []
+    }
+
+    add(data) {
+        this.children.push(new Node(data))
+    }
+
+    remove(data) {
+        this.children = this.children.filter(node => {
+            return node.data !== data
+        })
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null
+    }
+
+    traverseBF(fn) {
+        let arr = [this.root]
+
+        while (arr.length) {
+            const node = arr.shift()
+
+            arr.push(...node.children)
+            fn(node)
+        }
+    }
+
+    traverseDF(fn) {
+        let arr = [this.root]
+
+        while (arr.length) {
+            const node = arr.shift()
+
+            arr.unshift(...node.children)
+            fn(node)
+        }
+    }
+}
